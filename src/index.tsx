8b94c38d0060c77e9c56a59f6457a31ea90c3153
Jonny Lin <jonny@snippyly.com>
2023-02-15 21:07:38 -0800

    Added live cursor

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { SnippylyProvider } from '@snippyly/react';
+import { SnippylyProvider, SnippylyCursor } from '@snippyly/react';
 
 const rootContainer = document.getElementById('root');
 const root = createRoot(rootContainer as HTMLElement);
 root.render(
   <React.StrictMode>
     <SnippylyProvider apiKey="WDMgKshFEsPTqvBjUcH3">
+      <SnippylyCursor />
       <App />
     </SnippylyProvider>
   </React.StrictMode>
