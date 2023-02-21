393c00af7cd7a5ab3ad655a6f6908d1d96395a0b
Jonny Lin <jonny@snippyly.com>
2023-02-20 18:07:03 -0800

    Moved resetData cloud function to cleanup effect

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,19 @@ const App = () => {
 
     const isDataReset = window.sessionStorage.getItem('_snippyly_demo_reset');
 
-    if (isDataReset === null) {
-      fetch(
-        "https://us-central1-snippyly-sdk-prod.cloudfunctions.net/resetDemoData",
-        {
-          headers: { "Content-Type": "application/json" },
-          method: "POST",
-          body: JSON.stringify({ documentId: '' }),
-        }
-      );
-      window.sessionStorage.setItem('_snippyly_demo_reset', 'true');
+    return () => {
+      if (isDataReset === null) {
+        fetch(
+          "https://us-central1-snippyly-sdk-prod.cloudfunctions.net/resetDemoData",
+          {
+            headers: { "Content-Type": "application/json" },
+            method: "POST",
+            body: JSON.stringify({ documentId: '' }),
+          }
+        );
+        window.sessionStorage.setItem('_snippyly_demo_reset', 'true');
+      }
+
     }
   }, []);
 
